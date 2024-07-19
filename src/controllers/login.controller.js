870cab363c009404003e5b7e671cd938a2ac953b
TukaramM <machpalletukaram109@gmail.com>
2024-07-19 19:33:45 +0530

    Login and LogOut

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -3,6 +3,22 @@ import ApiError from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
 import ApiResponse from "../utils/ApiResponse.js"
 
+const generateAccessTokenAndRefreshToken = async (userId)=>{
+    try {
+        const user= await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshtoken = user.generateRefreshToken()
+
+        user.refreshtoken=refreshtoken
+        await user.save({validateBeforeSave:false})
+
+        return {accessToken,refreshtoken}
+
+    } catch (error) {
+        throw new ApiError(500 , "something went wrong while generating refresh and access token")
+    }
+}
+
 const loginUser = asynch (async(req,res)=>{
     //req body ->data
     // username or email
@@ -23,5 +39,59 @@ const loginUser = asynch (async(req,res)=>{
     if(!user){
         throw new ApiError(404,"user does not exist")
     }
+    // we alredy find user that why we use "user" instead of "User".
+    const ispasswordValid = await user.isPasswordCorrect(password)
+    if (!ispasswordValid) {
+        throw new ApiError(401,"password incorrect")
+    }
+    
+    const{accessToken,refreshtoken} =  await generateAccessTokenAndRefreshToken(user._id)
+    const loggInuser = await User.findById(user._id).select("-password -refreshtoken")
+
+    //cokkies 
+    const options= {
+        httpOnly:true, // cookie can manage bye server only that why we use httpOnly
+        secure:true
+    }
+    return res.status(200).cookie("accessToken" ,accessToken , options).cookie("refreshtoken" ,refreshtoken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                loggInuser,
+                user:accessToken,
+                refreshtoken
+            },
+            "User Logged in Successfully"
+        )
+    )
+    
 
 })
+const logout = asynch(async(req,res)=>{
+     await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshtoken:undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+    // Clear cookie 
+    const options ={
+        httpOnly:true,
+        secure:true
+    }
+    
+    return res.status(200).clearCookie("accessToken", options).clearCookie("refreshtoken",options)
+    .json(
+        new ApiResponse(200,{},"User looged out")
+    )
+    
+
+})
+
+export  {loginUser,logout}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import Router from "express";
 import registerUser from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
+import { loginUser, logout } from "../controllers/login.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router =Router()
 
@@ -19,5 +21,10 @@ router.route("/register").post(
 
     registerUser
 )
+
+router.route("/login").post(loginUser)
+
+//secure route
+router.route("/logout").post(verifyJWT,logout)
 
 export default router
